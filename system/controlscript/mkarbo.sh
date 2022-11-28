b07d48837c957df3cec8cc4648414ca5c76f4299
guillaume <guillaume.thevenot@epitech.eu>
2022-11-28 14:37:07 +0100

    first two parts of mkarbo.sh span tree

--- system/controlscript/mkarbo.sh
+++ system/controlscript/mkarbo.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+mkdir Script && mkdir Script/Rep_Perl && mkdir Script/Rep_Shell && mkdir Script/Rep_Shell/Rep_Sh && mkdir Script/Rep_Shell/Rep_Ksh && mkdir Script/Rep_Shell/Rep_Bash && mkdir Script/Python
+mkdir Temporaire && mkdir Temporaire/2019 && mkdir Temporaire/2019/1-semestre && mkdir Temporaire/2019/2-semestre && mkdir Temporaire/2020 && mkdir Temporaire/2020/1-semestre && mkdir Temporaire/2020/2-semestre
