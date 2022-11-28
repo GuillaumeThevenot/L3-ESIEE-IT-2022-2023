75f3038f83d3870609c2c50abe9bc4dccf7ffe24
guillaume <guillaume.thevenot@epitech.eu>
2022-11-28 14:49:05 +0100

    d

---
+++ system/controlscript/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -r $HOME/Script
+rm -r $HOME/Temporaire
+rm -r $HOME/Projet
--- system/controlscript/mkarbo.sh
+++ system/controlscript/mkarbo.sh
@@ -3,3 +3,7 @@
 mkdir $HOME/Script && mkdir $HOME/Script/Rep_Perl && mkdir $HOME/Script/Rep_Shell && mkdir $HOME/Script/Rep_Shell/Rep_Sh && mkdir $HOME/Script/Rep_Shell/Rep_Ksh && mkdir $HOME/Script/Rep_Shell/Rep_Bash && mkdir $HOME/Script/Python
 mkdir $HOME/Temporaire && mkdir $HOME/Temporaire/2019 && mkdir $HOME/Temporaire/2019/1-semestre && mkdir $HOME/Temporaire/2019/2-semestre && mkdir $HOME/Temporaire/2020 && mkdir $HOME/Temporaire/2020/1-semestre && mkdir $HOME/Temporaire/2020/2-semestre
 mkdir $HOME/Projet && mkdir $HOME/Projet/Projet-L3 && mkdir $HOME/Projet/Projet-M1 && mkdir $HOME/Projet/Projet-M2
+
+touch $HOME/Projet/Projet-L3/L3.txt
+touch $HOME/Projet/Projet-M1/M1.txt
+touch $HOME/Projet/Projet-M2/M2.txt
